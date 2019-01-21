30145f5673f489ef1dba4517f78512d37c116837
Abraham Hsu <smokiebacon@gmail.com>
2019-01-21 13:33:05 -0800

    connects to Mongo

---
+++ db/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const connectionString = 'mongodb://localhost/House';
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false //to get rid of Decprecation warning
+});
+
+mongoose.connection.on('connected', () => {
+    console.log(`Mongoose is connected to ${connectionString}`);
+})
+
+mongoose.connection.on('error', (err) => {
+    console.log(`Mongoose ERROR ${err}`);
+})
+
+mongoose.connection.on('disconnected', () => {
+    console.log(`Mongoose has DISCONNECTED from ${connectionString}`);
+})
